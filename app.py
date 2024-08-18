ed8ecd6388abfe502d0e7d280d549a0bcbe162df
eitan <eitanp2001@gmail.com>
2024-08-18 13:55:53 +0300

    fixed mysql in docker-compose

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}/{}'.format(
-    os.getenv('DB_USER', 'flask'),
+    os.getenv('DB_USER', 'mysql'),
     os.getenv('DB_PASSWORD', ''),
     os.getenv('DB_HOST', 'mysql'),
-    os.getenv('DB_NAME', 'flask')
+    os.getenv('DB_NAME', 'mysql')
 )
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
